7360cfd7150ee76f93781a554685b1e63ce8e5be
Kézily Correa Luchini <kezily@gmail.com>
2020-06-08 20:33:18 -0300

    validation with celebrate

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -23,5 +23,23 @@ module.exports = {
       })
 
       return res.json({ id });
+   },
+
+   async delete(req, res) {
+      const { id } = req.params;
+      const ong_id = req.headers.authorization;
+
+      const ongs = await connection('ongs')
+         .where('id', id)
+         .select('id')
+         .first();
+
+      if (ongs.id != ong_id) {
+         return res.status(401).json({ error: 'Operation not permitted.' });
+      }
+
+      await connection('ongs').where('id', id).delete();
+
+      return res.status(204).send();
    }
 }
